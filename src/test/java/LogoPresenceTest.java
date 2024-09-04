768afa0f1a82fe750a3963fda9504e59efea65e4
Art Ond <razenec@gmail.com>
2024-09-04 12:36:02 -0700

    TopUpBalanceTest class added

--- src/test/java/LogoPresenceTest.java
+++ src/test/java/LogoPresenceTest.java
@@ -8,31 +8,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LogoPresenceTest extends BaseTest {
     @Test
     public void testVisaLogoPresence() {
-        WebElement visaLogo = driver.findElement(By.xpath("//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/visa.svg' and @alt='Visa']"));
+        WebElement visaLogo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='Visa']"));
         assertTrue(visaLogo.isDisplayed(), "Visa logo is not displayed on the page");
     }
 
     @Test
     public void testVisaVerifiedLogoPresence() {
-        WebElement visaVerifiedLogo = driver.findElement(By.xpath("//img[@src='/local/templates/new_design/assets/html/images/pages/index/pay/visa-verified.svg' and @alt='Verified By Visa']"));
+        WebElement visaVerifiedLogo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='Verified By Visa']"));
+        String logoSrc = visaVerifiedLogo.getAttribute("src");
+        System.out.println(logoSrc);
         assertTrue(visaVerifiedLogo.isDisplayed(), "Verified By Visa logo is not displayed on the page");
     }
 
     @Test
     public void testMastercardLogoPresence() {
-        WebElement mastercardVerifiedLogo = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[3]/img"));
+        WebElement mastercardVerifiedLogo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='MasterCard']"));
         assertTrue(mastercardVerifiedLogo.isDisplayed(), "Mastercard logo is not displayed on the page");
     }
 
     @Test
     public void testMastercardSecureCodeLogoPresence() {
-        WebElement mastercardSecureCodeLogo = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[4]/img"));
+        WebElement mastercardSecureCodeLogo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='MasterCard Secure Code']"));
         assertTrue(mastercardSecureCodeLogo.isDisplayed(), "MastercardVerified logo is not displayed on the page");
     }
 
     @Test
     public void testBelcardLogoPresence() {
-        WebElement belcardLogo = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]/ul/li[5]/img"));
+        WebElement belcardLogo = driver.findElement(By.xpath("//section[@class='pay']//div[@class='pay__partners']//img[@alt='Белкарт']"));
         assertTrue(belcardLogo.isDisplayed(), "Belcard logo is not displayed on the page");
     }
 }
