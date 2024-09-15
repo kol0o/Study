0c53cc909fa168969d294c1e229fac9d2f07fb43
Art Ond <razenec@gmail.com>
2024-09-15 12:55:05 +0300

    small refactoring and allure setup

--- src/test/java/PaymentTest.java
+++ src/test/java/PaymentTest.java
@@ -9,11 +9,10 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PaymentTest extends BaseTest {
-    private MainPage mainPage;
 
     @Test
     public void testPaymentProcessForConnectionService() {
-        mainPage = new MainPage(driver);
+        MainPage mainPage = new MainPage(driver);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         // Шаг 1: Отклоняем всплывающее окно cookies
@@ -39,46 +38,36 @@ public class PaymentTest extends BaseTest {
         // Шаг 6: Проверяем правильность отображения суммы
         WebElement amountSpan = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='10.00 BYN']")));
         assertTrue(amountSpan.isDisplayed(), "Сумма 10.00 BYN не отображена корректно");
-        System.out.println("passed");
 
         WebElement payButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(), 'Оплатить  10.00 BYN')]")));
         assertTrue(payButton.isDisplayed(), "Кнопка 'Оплатить 10.00 BYN' не отображена корректно");
-        System.out.println("passed");
 
         // Шаг 7: Проверяем правильность введенного номера телефона
         WebElement phoneSpan = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(), 'Номер:375297777777')]")));
         assertTrue(phoneSpan.isDisplayed(), "Введенный номер телефона отображен некорректно");
-        System.out.println("passed");
 
         // Шаг 8: Проверяем placeholder'ы для незаполненных полей
         WebElement cardNumberLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text()='Номер карты']")));
         assertTrue(cardNumberLabel.isDisplayed(), "Надпись 'Номер карты' отсутствует");
-        System.out.println("passed");
 
         WebElement expiryDateLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text()='Срок действия']")));
         assertTrue(expiryDateLabel.isDisplayed(), "Надпись 'Срок действия' отсутствует");
-        System.out.println("passed");
 
         WebElement cvcLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text()='CVC']")));
         assertTrue(cvcLabel.isDisplayed(), "Надпись 'CVC' отсутствует");
-        System.out.println("passed");
 
         WebElement cardHolderLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[text()='Имя держателя (как на карте)']")));
         assertTrue(cardHolderLabel.isDisplayed(), "Надпись 'Имя держателя (как на карте)' отсутствует");
-        System.out.println("passed");
 
         // Шаг 9: Проверяем наличие иконок платежных систем
         WebElement visaIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@src='assets/images/payment-icons/card-types/visa-system.svg']")));
         assertTrue(visaIcon.isDisplayed(), "Иконка Visa не отображена");
-        System.out.println("passed");
 
         WebElement mastercardIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@src='assets/images/payment-icons/card-types/mastercard-system.svg']")));
         assertTrue(mastercardIcon.isDisplayed(), "Иконка MasterCard не отображена");
-        System.out.println("passed");
 
         WebElement belcardIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@src='assets/images/payment-icons/card-types/belkart-system.svg']")));
         assertTrue(belcardIcon.isDisplayed(), "Иконка Белкарт не отображена");
-        System.out.println("passed");
 
         // Шаг 10: Проверяем наличие случайной иконки из двух возможных
         WebElement cardsBrands = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".cards-brands")));
